1eb5ce2e323c9a74efc1fcd83257e75f17c18d3d
yuhangsun1014 <yuhangsun1014@gmail.com>
2025-03-12 15:59:18 -0700

    properly formatted css file

--- public/uw-student-marketplace/src/components/UserProfile.jsx
+++ public/uw-student-marketplace/src/components/UserProfile.jsx
@@ -1,13 +1,18 @@
 import React, { useState,useEffect } from "react";
-import { getAuth,onAuthStateChanged } from "firebase/auth";
+import { getAuth,onAuthStateChanged, updateProfile } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "../Firebase-config";
+import { db, storage } from "../Firebase-config";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { use } from "react";
+import './UserProfile.css';
 
 function UserProfile(){
     const [displayName, setDisplayName] = useState("Guest");
     const [userEmail, setUserEmail] = useState("");
     const [createDate, setCreateDate] = useState("");
-
+    const [profilePic, setProfilePic] = useState(null);
+    const [uploading, setUploading] = useState(false);
+    const [previewURL, setPreviewURL] = useState(null);
     useEffect(() => {
         const auth = getAuth();
         const userSignInState = onAuthStateChanged(auth, async(user)=>{
@@ -26,12 +31,92 @@ function UserProfile(){
         });
         return userSignInState;
     },[]);
+
+const handleFileChange = async(e)=>{
+    if(e.target.files && e.target.files[0]){
+        const file = e.target.files[0];
+        setProfilePic(file);
+        const url = URL.createObjectURL(file);
+        setPreviewURL(url);
+        
+    }
+    
+}
+
+const handleProfilePic = async(event)=>{
+    event.preventDefault();
+
+    // form validation
+    if (!profilePic) {
+        alert("Please upload a product image");
+        return;
+    }
+
+    setUploading(true);
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+
+    if (!user) {
+        alert("No user is logged in.");
+        setUploading(false);
+        return;
+    }
+
+    const fileRef = ref(
+        storage,`profilePictures/${user.uid}/${Date.now()}-${profilePic.name}`
+    );
+
+    try{
+        const uploadTask = uploadBytesResumable(fileRef,profilePic);
+        uploadTask.on(
+            "state_changed",
+            (snapshot)=>{
+                const progress = (snapshot.bytesTransferred/ snapshot.totalBytes) *100;
+                console.log(`upload is ${progress}% done`);
+            },
+            (error) => {
+                console.error("Uploade error", error);
+                alert("Error uploading profile picture");
+                setUploading(false);
+            },
+            async() =>{
+                const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                console.log("Download URL: ", downloadURL);
+
+                await updateProfile(user,{photoURL: downloadURL});
+                alert("User profile updated successfully");
+                setUploading(false);
+                setProfilePic(null);
+            }
+            
+        );
+    }catch(error){
+        console.error("Error during upload:", error);
+        alert("Upload failed: " + error.message);
+        setUploading(false);        
+    }
+    
+};
+
+
+
     return(
         <div className="user-profile-container">
             {/**Left side, Avatar + Name */}
             <div className="profile-left">
-                <img src="https://pga-tour-res.cloudinary.com/image/upload/c_thumb,g_face,w_280,h_350,z_0.7/headshots_08793.jpg" className="profile-pic" />
+                <img src={previewURL} className="profile-pic" alt="profile"/>
                 <h1 className="profile-name">{displayName}</h1>
+                <form onSubmit={handleProfilePic}>
+                    <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleFileChange}
+                    />
+                    <button type="submit" disabled={uploading}>
+                    {uploading ? "Uploading..." : "Update Profile Picture"}
+                    </button>
+                </form>
             </div>
             <div className="profile-right">
                 <div className="contact-info">
