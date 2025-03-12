997a2a6dc1db16aa45bebe6e0cdcf7656160357a
yuhangsun1014 <yuhangsun1014@gmail.com>
2025-03-11 17:10:15 -0700

    formatted ItemPosted page and set it to show up if the whole submission is uploaded, will add a button to return to main menu

---
+++ public/uw-student-marketplace/src/pages/ItemPostedPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './ItemPostedPage.css';
+import MiddleSection from "../components/MiddleSection";
+function ItemPostedPage() {
+    return (
+        <div>
+            <div className="app-container">
+                <MiddleSection>
+                    <div className="successful-image">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/768px-Eo_circle_green_checkmark.svg.png?20200417132424" className="checkmark" />
+                        <h1 className="Posted-message">Item Posted!</h1>
+                        <button></button>
+                    </div>
+                </MiddleSection>
+            </div>
+        </div>
+    );
+}
+export default ItemPostedPage;
